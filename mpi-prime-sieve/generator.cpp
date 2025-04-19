10a7c94aba5c2b084261f4f71684b0a967381328
yuhaopro <yuhao9583@gmail.com>
2025-04-19 19:10:39 +0100

    chore: rebuild and comment out logging statements

--- mpi-prime-sieve/generator.cpp
+++ mpi-prime-sieve/generator.cpp
@@ -10,19 +10,19 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
 
     // will execute siever process who belongs in the nextComm group.
-    std::cout << "Generator: Spawning Siever" << std::endl;
+    //std::cout << "Generator: Spawning Siever" << std::endl;
     MPI_Comm_spawn("siever", argv, 1, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &nextComm, MPI_ERRCODES_IGNORE);
     while (candidate < N)
     {
         // first candidate = 2, known prime number
-        std::cout << "Generator: Sending Candidate " << candidate << std::endl;
+        //std::cout << "Generator: Sending Candidate " << candidate << std::endl;
         MPI_Send(&candidate, MESSAGE_COUNT, MPI_INT, siever_rank, siever_tag, nextComm);
         candidate++;
     }
     candidate = -1;
 
     // To signal end of program
-    std::cout << "Generator: Sending Terminal Signal" << std::endl;
+    //std::cout << "Generator: Sending Terminal Signal" << std::endl;
     MPI_Send(&candidate, MESSAGE_COUNT, MPI_INT, siever_rank, siever_tag, nextComm);
     MPI_Finalize();
 }
